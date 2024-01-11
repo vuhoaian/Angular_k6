5be3aa797508e31b19326583fd2e4376beb91d0e
vuhoaian <vuhoaian1804@gmail.com>
2024-01-11 19:31:06 +0700

    admin

--- angularr/angular1/src/app/app.routes.ts
+++ angularr/angular1/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import{ ProductsComponent} from'./pages/admin/products/products.component'
-
+import{AdminComponent} from'./layouts/admin/admin.component'
 export const routes: Routes = [
 // route '/' = page Home
   // path, component
   { path: '', component: HomeComponent },
-  { path: 'admin', component: ProductsComponent },
+ //{ path: 'admin', component: ProductsComponent },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    children: [{ path: 'products', component: ProductsComponent }],
+  },
 ];
---
+++ angularr/angular1/src/app/layouts/admin/admin.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-admin',
+  standalone: true,
+  imports: [RouterOutlet, SidebarComponent],
+  templateUrl: './admin.component.html',
+  styleUrl: './admin.component.css',
+})
+export class AdminComponent {
+  
+}
